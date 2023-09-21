f3a4753da117b5ddf302df91cae96a76ca9ad95b
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-09-22 08:23:25 +1200

    added in auth ensuring through a JWT token

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-const http = require('http');
 const Gpio = require('onoff').Gpio;
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken'); // Add the JWT library
+const express = require('express');
 
-const hostname = '192.168.0.168';
-const port = 3000;
-const relayPin = 17;
+// Load environment variables from .env file
+dotenv.config();
+
+// Get the env variables we need
+const { hostname, port, relayPin, jwtSecret } = process.env; // Provide the JWT secret
 
 // Initialize the relay pin as an output
 const relay = new Gpio(relayPin, 'out');
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.statusCode = 200;
+const app = express();
 
-  if (req.url === '/on') {
-    // Turn the relay on
-    relay.writeSync(1);
-    res.end('Relay is ON\n');
-  } else if (req.url === '/off') {
-    // Turn the relay off
-    relay.writeSync(0);
-    res.end('Relay is OFF\n');
-  } else {
-    res.end('Unknown command\n');
-  }
+app.use(express.json()); // Enable JSON request parsing
+
+// Relay control routes
+app.get('/on', ensureAuthenticated, (req, res) => {
+  // Turn the relay on
+  relay.writeSync(1);
+  res.send('Relay is ON');
 });
 
-server.listen(port, hostname, () => {
+app.get('/off', ensureAuthenticated, (req, res) => {
+  // Turn the relay off
+  relay.writeSync(0);
+  res.send('Relay is OFF');
+});
+
+// Ensure user is authenticated by verifying the JWT token
+function ensureAuthenticated(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send('Unauthorized. Token missing.');
+  }
+
+  try {
+    // Verify the JWT token using the secret
+    const decoded = jwt.verify(token, jwtSecret);
+
+    // Access the user's claims in the 'decoded' object
+    if (decoded && decoded.permissions.includes('control_relay')) {
+      return next();
+    } else {
+      return res.status(403).send('Forbidden. Insufficient permissions.');
+    }
+  } catch (error) {
+    console.error('Token verification error:', error);
+    return res.status(401).send('Unauthorized. Invalid token.');
+  }
+}
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
