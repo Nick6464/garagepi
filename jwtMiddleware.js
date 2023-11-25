ccf9c611bc6b0a6f5bdbbff95871f217d5b960e9
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-11-25 21:46:28 +1300

    trying to sort jwt tokens

--- jwtMiddleware.js
+++ jwtMiddleware.js
@@ -45,7 +45,8 @@ async function getSigningKey(jwksUri, kid) {
 }
 
 const jwtVerificationMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  console.log(req.headers.authorization);
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ error: "Unauthorized - Token missing" });
