818fb21dc1fc772507d3f0e08f864c3f8cb9b87a
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-11-25 20:16:22 +1300

    Made press the only active end point and updated JWT verifying

--- jwtMiddleware.js
+++ jwtMiddleware.js
@@ -4,46 +4,42 @@ require("dotenv").config();
 
 const { AZURE_AD_TENANT_ID } = process.env;
 
-// Function to verify a JWT token
 async function verifyJwtToken(token) {
-  let publicKey;
   try {
-    publicKey = await fetchPublicKey();
-  } catch (error) {
-    console.error("Error fetching public key:", error);
-  }
+    const jwksUri = `https://login.microsoftonline.com/${AZURE_AD_TENANT_ID}/discovery/v2.0/keys`;
 
-  try {
-    const decoded = jwt.verify(token, publicKey, { algorithms: ["RS256"] });
-    // If the token is valid, 'decoded' will contain the token payload
-    // console.log('JWT verification succeeded');
-    console.log("JWT verification succeeded:", decoded);
+    const signingKey = await getSigningKey(
+      jwksUri,
+      jwt.decode(token, { complete: true }).header.kid
+    );
+    const decoded = jwt.verify(token, signingKey, { algorithms: ["RS256"] });
+    console.log("JWT verification succeeded");
     return decoded;
   } catch (error) {
-    // If the token is invalid or has expired, an error will be thrown
     console.error("JWT verification failed:", error.message);
     return null;
   }
 }
 
-async function fetchPublicKey() {
-  const opt = (
-    await axios.get(
-      `https://login.microsoftonline.com/${AZURE_AD_TENANT_ID}/v2.0/.well-known/openid-configuration`
-    )
-  ).data;
-
-  // Use the jwks_uri to get the signing keys
-  const jwksResponse = await axios.get(opt.jwks_uri);
-  const jwks = jwksResponse.data;
-
-  // Extract the X.509 certificate
-  const x5c = jwks.keys[0].x5c[0];
-
-  // Parse the X.509 certificate into a usable public key format (PEM)
-  const publicKeyPem = `-----BEGIN CERTIFICATE-----\n${x5c}\n-----END CERTIFICATE-----`;
-
-  return publicKeyPem;
+async function getSigningKey(jwksUri, kid) {
+  try {
+    const client = jwksClient({
+      jwksUri,
+    });
+    const key = await new Promise((resolve, reject) => {
+      client.getSigningKey(kid, (err, key) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(key.getPublicKey());
+        }
+      });
+    });
+    return key;
+  } catch (error) {
+    console.error("Error fetching signing key:", error);
+    throw error;
+  }
 }
 
 const jwtVerificationMiddleware = async (req, res, next) => {
