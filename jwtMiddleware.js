75d7b2776aad6fa867fb85922954475e1fe28048
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-11-25 22:00:23 +1300

    Update jwtMiddleware.js

--- jwtMiddleware.js
+++ jwtMiddleware.js
@@ -45,8 +45,8 @@ async function getSigningKey(jwksUri, kid) {
 }
 
 const jwtVerificationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  const token = req.headers.authorization;
+  // console.log(req.headers.authorization);
+  const token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ error: "Unauthorized - Token missing" });
